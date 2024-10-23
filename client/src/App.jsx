cf68c6c37c1c9d1f1f837f0c6c894fbfd8a4c656
ryantran <113406848+rryantran@users.noreply.github.com>
2024-10-22 19:35:18 -0500

    set up express and verify it works with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <></>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Async function to fetch test data from the server
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/test");
+
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // Call the async function
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>{data}</h1>
+    </>
+  );
 };
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express(); // Create an Express app
+const port = 3000; // Set the port
+
+app.use(cors()); // Enable CORS
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Test route
+app.get("/test", (request, response) => {
+  response.send("I love Panda Express");
+});
