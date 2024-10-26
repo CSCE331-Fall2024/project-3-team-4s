008eab9bc23af15ee7b48daffb9f3e7b4c38581d
EshwarReddy13 <eshwarreddygadi@gmail.com>
2024-10-26 17:21:11 -0500

    Added a nav bar and a background to the page

--- client/src/pages/CustomerHome.jsx
+++ client/src/pages/CustomerHome.jsx
@@ -1,7 +1,23 @@
+import React from 'react';
+import './styles/CustomerHome.css';
+import logo from '../customerImages/logo.png';
 const CustomerHome = () => {
   return (
-    <div>
-      <h1>Customer</h1>
+    <div className="navbar">
+      <img src={logo} alt="Logo" className="navbar-logo" />
+      <div className="navbar-links">
+        <a href="#">Home</a>
+        <span className="divider">|</span>
+        <a href="#">About</a>
+        <span className="divider">|</span>
+        <a href="#">Services</a>
+        <span className="divider">|</span>
+        <a href="#">Our Rewards</a>
+      </div>
+      <div className="navbar-actions">
+        <button className="navbar-button">ORDER</button>
+        <span role="img" aria-label="user" className="navbar-icon">👤</span>
+      </div>
     </div>
   );
 };
