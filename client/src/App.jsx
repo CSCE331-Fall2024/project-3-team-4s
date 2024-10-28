b87bdedd6d1f572923ce4b0ffd14306cb6e2abf6
ryantran <113406848+rryantran@users.noreply.github.com>
2024-10-27 23:48:23 -0500

    set up files for employee management page + create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import CustomerHome from "./pages/CustomerHome";
 import ManagerHome from "./pages/ManagerHome";
 import CashierHome from "./pages/CashierHome";
+import Employees from "./pages/Employees";
 
 const App = () => {
   useEffect(() => {
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/" element={<CustomerHome />} />
         <Route path="/manager" element={<ManagerHome />} />
         <Route path="/cashier" element={<CashierHome />} />
+        <Route path="/employees" element={<Employees />} />
       </Routes>
     </>
   );
---
+++ client/src/pages/Employees.jsx
@@ -0,0 +1,12 @@
+import "./Employees.css";
+import PageHeader from "../components/PageHeader";
+
+const Employees = () => {
+  return (
+    <div className="employees-container">
+      <PageHeader pageTitle="Employees" />
+    </div>
+  );
+};
+
+export default Employees;
