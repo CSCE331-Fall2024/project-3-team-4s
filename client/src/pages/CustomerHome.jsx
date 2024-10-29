55292e43c76b5950ba8acf0f59370369500eff59
EshwarReddy13 <eshwarreddygadi@gmail.com>
2024-10-29 11:28:07 -0500

    Added addtionaly functionality so that the user can select multiple entrees

--- client/src/pages/CustomerHome.jsx
+++ client/src/pages/CustomerHome.jsx
@@ -352,8 +352,6 @@ const CustomerHome = () => {
                     
                     
                     
-                    
-
   const handleBackToMenu = () => {
     setSelectedItem(null);
     setSides([]);
