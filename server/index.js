fc1f07741b15a825e446454962416138432a6d53
Landon Uelsmann <epicgames72200@gmail.com>
2024-10-29 19:44:20 -0500

    Inventory table is complete and shows all of the ingredients in store. The functionality for add, edit, and remove will be added in the next sprint.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import kioskRoutes from "./routes/kioskRoutes.js";
+import inventoryRoutes from "./routes/inventoryRoutes.js";
 
 const app = express(); // Create an Express app
 const port = 3000; // Set the port
 
 app.use(cors()); // Enable CORS
 app.use("/kiosk", kioskRoutes); // Use the kiosk routes
+app.use("/inventory", inventoryRoutes); // Use the inventory routes
 
 // Start the server
 app.listen(port, () => {
